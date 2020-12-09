be408066dc5fe954b006c1e44dbfccdbd7264b98
zeyneppinar <zeynepinarkaya@gmail.com>
2020-12-09 16:29:29 +0300

    init

---
+++ js/model.js
@@ -0,0 +1,28 @@
+class Model {
+    constructor() {
+        this.numberOfColors = 4;
+        this.score = 0;
+        //
+        //
+        this._musicOn = true;
+        this._sfxOn = true;
+        this.gameTitle="Star\nCollector\n";
+        this.instructionText="Click left or right of the ball \n to move. Try not to fall. \n Collect stars for points.";
+    }
+    set musicOn(val) {
+        this._musicOn = val;
+        console.log(val);
+        mt.emitter.emit(mt.constants.MUSIC_CHANGED);
+    }
+    get musicOn() {
+        return this._musicOn;
+    }
+    set sfxOn(val) {
+        this._sfxOn = val;
+        console.log(val);
+        mt.emitter.emit(mt.constants.SOUND_CHANGED);
+    }
+    get sfxOn() {
+        return this._sfxOn;
+    }
+}
